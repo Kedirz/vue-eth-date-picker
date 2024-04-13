cd0dbf4a21d81be48c41eecaa964062758404022
Kedir Zeinu <kedirmohammedzeinu@gmail.com>
2024-04-13 17:24:24 -0400

    Adds: geez days, preserve white space compiler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,62 @@
 <template>
-  <div class="header-wrapper">
-    <button>
-      <i class="icon-chevron-left"></i>
-    </button>
-    <div class="month-year">
-      <div class="month-list">{{ selectedDate.getMonth() }}</div>
-      <div class="month-"></div>
+  <div class="wrapper">
+    <div class="header-wrapper">
+      <button>
+        <i class="icon-chevron-left"></i>
+      </button>
+      <div class="month-year">
+        <span @click="showMonths = true">{{ geezMonths[selectedDateGeez.month - 1] }}</span>
+        <span @click="showYears = true">{{ selectedDateGeez.year }}</span>
+      </div>
+      <button>
+        <i class="icon-chevron-right"></i>
+      </button>
     </div>
-    <button>
-      <i class="icon-chevron-right"></i>
-    </button>
   </div>
+  <div class="days">
+    <div v-for="day in geezDays">
+      <p>{{ day.slice(0, 2) }}</p>
+    </div>
+  </div>
+  <div class="dates"></div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ge } from 'ethiopic-calendar'
+import { geezDaysEnums, geezMonthsEnums } from '@/enums/geez.dates'
 
 export default defineComponent({
   name: 'HeaderComp',
   data() {
     return {
-      selectedDate: new Date(new Date().setHours(0, 0, 0, 0))
+      selectedDate: new Date(new Date().setHours(0, 0, 0, 0)),
+      geezMonths: geezMonthsEnums,
+      geezDays: geezDaysEnums,
+      showMonths: false,
+      showYears: false
     }
   },
-  created() {
-    this.selectedDate = new Date()
+  methods: {
+    handleClickMonthNavigations(direction: 1 | -1) {
+      const curMonth = selectedDateGeez.month
+      if (direction === 1) {
+        // FORWARD: i.e. next month
+        if (curMonth > 13) {
+          //
+        } else {
+          //
+        }
+      } else {
+        // BACKWARD; i.e. previous month
+      }
+    }
   },
   computed: {
     selectedDateGeez() {
-      console.log(ge(this.selectedDate))
-      return ge(this.selectedDate)
+      const year = this.selectedDate.getFullYear(),
+        month = this.selectedDate.getMonth() + 1,
+        date = this.selectedDate.getDate()
+      return ge(year, month, date)
     }
   }
 })
@@ -38,5 +65,11 @@ export default defineComponent({
 .header-wrapper {
   flex-direction: row;
   display: flex;
+  gap: 8px;
+}
+.days {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
 }
 </style>
