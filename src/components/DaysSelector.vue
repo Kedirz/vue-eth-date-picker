fdb1df461398d0733cb6afcd2d48628c73c886c1
Kedir Zeinu <kedirmohammedzeinu@gmail.com>
2024-10-26 18:58:16 -0400

    Adds pagume num of days on month selector

--- src/components/DaysSelector.vue
+++ src/components/DaysSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <select v-model="selectedDate">
-    <option v-for="n in 30" :value="n" :key="n">
+    <option v-for="n in numOfDaysInMonth" :value="n" :key="n">
       {{ n }}
     </option>
   </select>
@@ -8,6 +8,7 @@
 <script lang="ts" setup>
 import { store, updateStore } from '@/store'
 import { computed } from 'vue'
+import { getNumberOfDaysInEthiopianMonth } from './utils/date'
 
 const selectedDate = computed({
   set(newValue: number) {
@@ -16,5 +17,13 @@ const selectedDate = computed({
   get() {
     return store.selectedDate
   }
+})
+
+const numOfDaysInMonth = computed(() => {
+  const numOfDays = getNumberOfDaysInEthiopianMonth({
+    year: store.selectedYear,
+    month: store.selectedMonth
+  })
+  return numOfDays
 })
 </script>
