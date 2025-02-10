44fbb89ebc94aaf112ec660e9b6dd16a410ad949
Kedir Zeinu <kedirmohammedzeinu@gmail.com>
2025-02-10 18:10:53 -0500

    copies ethiopic-calendar package into in app utils

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <HeaderComp />
+  <form @submit.prevent="onSubmit">
+    <MonthSelector />
+    <YearsSelector />
+    <DaysSelector />
+    <button class="ec-enter" type="submit">Enter</button>
+  </form>
 </template>
-<script>
-import { defineComponent } from 'vue'
-import HeaderComp from './components/Header.vue'
-export default defineComponent({
-  components: {
-    HeaderComp
+<script lang="ts" setup>
+import { store } from '@/store'
+import DaysSelector from '@/components/DaysSelector.vue'
+import MonthSelector from '@/components/MonthSelector.vue'
+import YearsSelector from '@/components/YearsSelector.vue'
+
+const props = defineProps<{
+  modelValue: {
+    year: number
+    month: number
+    date: number
   }
-})
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: { year: number; month: number; date: number }): void
+}>()
+const onSubmit = () => {
+  emit('update:modelValue', {
+    year: store.selectedYear,
+    month: store.selectedMonth,
+    date: store.selectedDate
+  })
+}
 </script>
